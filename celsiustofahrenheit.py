3ad6db285ab22cc7fa5377dfcc666f7b00e82695
M0N4K0 <myriamhamed@limitlessinitiatives.com>
2016-06-13 22:28:40 +0100

    celsius to fahrenheit converter

---
+++ celsiustofahrenheit.py
@@ -0,0 +1,4 @@
+temperature_celsius = raw_input('What is the temperature in Celsius?\n')
+temperature_fahrenheit = float(temperature_celsius) * 1.8 + 32
+print 'That is ' + str(temperature_fahrenheit) + ' Fahrenheit'
+
