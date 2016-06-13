fea19c5bc2934713bd1ef6d48ef0841a67166e00
M0N4K0 <myriamhamed@limitlessinitiatives.com>
2016-06-13 23:33:11 +0100

    using string interpolation

--- grosspaycalculator.py
+++ grosspaycalculator.py
@@ -1,10 +1,10 @@
-print 'Hello there, my name is Pyt and I will be assisting you with your salary calculations'
+print 'Hello there, my name is Pyt and I will be assisting you with your salary calculations.'
 name = raw_input('First of all, what is your name?\n')
-print 'Hi ' + name + ', lovely to meet you'
+print 'Hi %s, lovely to meet you.' % name
 hours_week = float(raw_input('How many hours a week do you work?\n'))
 print 'Thanks!'
 rate = float(raw_input('What is your hourly rate?\n'))
 print 'Thanks! I will calculate the results now.'
 pay_week = hours_week * rate
-print 'Your weekly gross pay is:',str(pay_week)
+print 'Your weekly gross pay is: %.2f' % pay_week
 
